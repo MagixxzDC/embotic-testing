816c6173fbd3e19ff785d05f085737d8d1103ae8
Magixxz <dgarrett234@gmail.com>
2025-01-27 18:45:35 -0500

    Update

--- commands/cheese.js
+++ commands/cheese.js
@@ -1,7 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'cheese',
     description: 'Sends an infinite stream of cheese emojis.',
     async execute(message, args) {
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('You do not have permission to use this command.')
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            return message.reply({ embeds: [errorEmbed] });
+        }
+
         if (args[0] === '-stop') {
             if (message.client.cheeseInterval) {
                 clearInterval(message.client.cheeseInterval);
