013709af22e3f29a64c8901e2a5bee935b354bd0
Magixxz <dgarrett234@gmail.com>
2025-01-27 17:28:52 -0500

    Update

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,7 +10,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setColor('#141414')
-            .setTitle('User Information :person:')
+            .setTitle('User Information :personembotic:')
             .setDescription(`This is all the information available on ${user.tag}`)
             .setThumbnail(user.displayAvatarURL({ dynamic: true }))
             .addFields(
