82f4a6bc2570fc79176f44c184ac98de1d8bed3b
Magixxz <dgarrett234@gmail.com>
2025-01-27 17:10:47 -0500

    Update

--- commands/joke.js
+++ commands/joke.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = {
     name: 'joke',
