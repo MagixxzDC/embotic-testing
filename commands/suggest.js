a927831787601801a210a045dcd790dc5170d11c
Magixxz <dgarrett234@gmail.com>
2025-01-27 17:41:20 -0500

    Update

---
+++ commands/suggest.js
@@ -0,0 +1,72 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'suggest',
+    aliases: ['suggestion', 'feedback'],
+    description: 'Submit a suggestion.',
+    async execute(message, args) {
+        const suggestionChannelId = '1333566583875965019'; 
+        const suggestionChannel = message.guild.channels.cache.get(suggestionChannelId);
+
+        if (!suggestionChannel) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('Suggestion channel not found.')
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            return message.reply({ embeds: [errorEmbed] }).then(msg => {
+                setTimeout(() => msg.delete(), 30000);
+            });
+        }
+
+        const suggestion = args.join(' ');
+        if (!suggestion) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('Please provide a suggestion.')
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            return message.reply({ embeds: [errorEmbed] }).then(msg => {
+                setTimeout(() => msg.delete(), 30000);
+            });
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor('#141414')
+            .setTitle('New Suggestion')
+            .setDescription(suggestion)
+            .addFields(
+                { name: 'Suggested by', value: message.author.tag, inline: true },
+                { name: 'User ID', value: message.author.id, inline: true }
+            )
+            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+            .setTimestamp();
+
+        try {
+            const botMessage = await suggestionChannel.send({ embeds: [embed] });
+            const replyMessage = await message.reply('Thank you for your suggestion!');
+            
+            setTimeout(() => {
+                message.delete().catch(console.error);
+                botMessage.delete().catch(console.error);
+                replyMessage.delete().catch(console.error);
+            }, 30000);
+        } catch (error) {
+            console.error(error);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('There was an error trying to submit your suggestion.')
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            message.reply({ embeds: [errorEmbed] }).then(msg => {
+                setTimeout(() => msg.delete(), 30000);
+            });
+        }
+    },
+};
