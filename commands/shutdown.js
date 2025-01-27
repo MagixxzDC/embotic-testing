795ba1025279b8d40f0abf2da3fb82b5da791ead
Magixxz <dgarrett234@gmail.com>
2025-01-27 18:59:49 -0500

    Update

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'shutdown',
@@ -7,14 +7,14 @@ module.exports = {
         const authorizedUserId = '328248785933434881';
         
         if (message.author.id !== authorizedUserId) {
-            const embed = new MessageEmbed()
+            const embed = new EmbedBuilder()
                 .setColor('#141414')
                 .setTitle('Unauthorized')
                 .setDescription('You are not authorized to use this command.');
             return message.channel.send({ embeds: [embed] });
         }
 
-        const embed = new MessageEmbed()
+        const embed = new EmbedBuilder()
             .setColor('#141414')
             .setTitle('Shutdown')
             .setDescription('Shutting down...');
