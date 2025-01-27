da3a9aba7eacca91b207822ed5b62c91e631c761
Magixxz <dgarrett234@gmail.com>
2025-01-27 18:41:23 -0500

    Update

--- commands/cheese.js
+++ commands/cheese.js
@@ -2,15 +2,27 @@ module.exports = {
     name: 'cheese',
     description: 'Sends an infinite stream of cheese emojis.',
     async execute(message, args) {
+        if (args[0] === '-stop') {
+            if (message.client.cheeseInterval) {
+                clearInterval(message.client.cheeseInterval);
+                message.channel.send('The infinite cheese has been stopped.');
+                message.client.cheeseInterval = null;
+            } else {
+                message.channel.send('There is no infinite cheese running.');
+            }
+            return;
+        }
+
         message.channel.send('BEHOLD THE INFINITE CHEESE');
 
-        const interval = setInterval(() => {
+        message.client.cheeseInterval = setInterval(() => {
             message.channel.send(':cheese:').catch(console.error);
         }, 3000);
 
-        // Stop the interval after 1 minute to prevent spamming
+        // Stop the interval after 20 minutes to prevent spamming
         setTimeout(() => {
-            clearInterval(interval);
+            clearInterval(message.client.cheeseInterval);
+            message.client.cheeseInterval = null;
         }, 1200000);
     },
 };
