93eff95a3b86db97cf62247c50ab77a6d89d2111
Magixxz <dgarrett234@gmail.com>
2025-01-27 17:52:05 -0500

    Update

--- commands/suggest.js
+++ commands/suggest.js
@@ -5,14 +5,29 @@ module.exports = {
     aliases: ['suggestion', 'feedback'],
     description: 'Submit a suggestion.',
     async execute(message, args) {
-        const suggestionChannelId = '1333566583875965019'; 
+        const suggestionChannelId = '758365398587408386'; 
+        const suggestionSubmissionChannelId = '1333566583875965020';
         const suggestionChannel = message.guild.channels.cache.get(suggestionChannelId);
+        const suggestionSubmissionChannel = message.guild.channels.cache.get(suggestionSubmissionChannelId);
 
-        if (!suggestionChannel) {
+        if (message.channel.id !== suggestionChannelId) {
             const errorEmbed = new EmbedBuilder()
                 .setColor('#141414')
                 .setTitle('Error')
-                .setDescription('Suggestion channel not found.')
+                .setDescription('You can only use this command in the #suggestions channel.')
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            return message.reply({ embeds: [errorEmbed] }).then(msg => {
+                msg.delete();
+            });
+        }
+
+        if (!suggestionSubmissionChannel) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('Suggestion submission channel not found.')
                 .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
                 .setTimestamp();
 
@@ -47,7 +62,7 @@ module.exports = {
             .setTimestamp();
 
         try {
-            const botMessage = await suggestionChannel.send({ embeds: [embed] });
+            const botMessage = await suggestionSubmissionChannel.send({ embeds: [embed] });
             const replyMessage = await message.reply('Thank you for your suggestion!');
             
             message.delete().catch(console.error);
