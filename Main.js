21ba2dfbc8344ceec0b358896f9885c11241fb53
Magixxz <dgarrett234@gmail.com>
2025-01-27 15:01:23 -0500

    Update

--- Main.js
+++
@@ -1,9 +0,0 @@
-let btnEl = document.querySelector(".generate");
-let serialEl = document.querySelector(".serial");
-let discord = document.querySelector(".Discord");
-
-btnEl.onclick = function (event) {
-  event.target.parentNode.removeChild(event.target)
-    const randomSerial  = EMBOTICKEY;
-        serialEl.innerHTML = randomSerial;
-};
