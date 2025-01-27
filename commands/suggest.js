eb20d91779c97b408a9a5a606975b624eac84b3b
Magixxz <dgarrett234@gmail.com>
2025-01-27 18:02:32 -0500

    Update

--- commands/suggest.js
+++ commands/suggest.js
@@ -6,7 +6,9 @@ module.exports = {
     description: 'Submit a suggestion.',
     async execute(message, args) {
         const suggestionChannelId = '758365398587408386'; 
+        const suggestionSubmissionChannelId = '1333566583875965019'; 
         const suggestionChannel = message.guild.channels.cache.get(suggestionChannelId);
+        const suggestionSubmissionChannel = message.guild.channels.cache.get(suggestionSubmissionChannelId);
 
         if (message.channel.id !== suggestionChannelId) {
             const errorEmbed = new EmbedBuilder()
@@ -22,11 +24,11 @@ module.exports = {
             });
         }
 
-        if (!suggestionChannel) {
+        if (!suggestionSubmissionChannel) {
             const errorEmbed = new EmbedBuilder()
                 .setColor('#141414')
                 .setTitle('Error')
-                .setDescription('Suggestion channel not found.')
+                .setDescription('Suggestion submission channel not found.')
                 .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
                 .setTimestamp();
 
@@ -63,7 +65,7 @@ module.exports = {
             .setTimestamp();
 
         try {
-            const botMessage = await suggestionChannel.send({ embeds: [embed] });
+            const botMessage = await suggestionSubmissionChannel.send({ embeds: [embed] });
             const replyMessage = await message.reply('Thank you for your suggestion!');
             
             setTimeout(() => message.delete().catch(console.error), 0);
