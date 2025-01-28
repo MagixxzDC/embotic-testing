acc5be948d21ad8080957ef9661d17348e2a7b3a
Magixxz <dgarrett234@gmail.com>
2025-01-27 19:34:16 -0500

    Update

--- commands/report.js
+++ commands/report.js
@@ -24,7 +24,7 @@ module.exports = {
         });
 
         const embed = {
-            color: '#141414',
+            color: 0x141414, // Use an integer value for color
             title: 'Ticket Created',
             description: 'Please describe your issue in detail. A staff member will be with you shortly.',
             timestamp: new Date(),
