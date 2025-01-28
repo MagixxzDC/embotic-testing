869519362e080f8dce4d3583f191c593fb06db30
Magixxz <dgarrett234@gmail.com>
2025-01-27 23:00:35 -0500

    Update

--- commands/purge.js
+++ commands/purge.js
@@ -2,80 +2,48 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'purge',
-    aliases: ['p', 'clear', 'c', 'delete', 'd'],
-    description: 'Deletes a specified number of messages from the channel.',
+    aliases: ['clear', 'clean'],
+    description: 'Deletes a specified number of messages from a channel.',
     async execute(message, args) {
-        if (!message.member.permissions.has('MANAGE_MESSAGES')) {
-            return message.reply('You do not have permission to use this command.');
+        const amount = parseInt(args[0]);
+
+        if (isNaN(amount) || amount < 1 || amount > 100) {
+            const embed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Invalid Amount')
+                .setDescription('Please provide a number between 1 and 100.');
+            return message.channel.send({ embeds: [embed] });
         }
 
-        const filter = response => {
-            return response.author.id === message.author.id;
-        };
+        try {
+            const fetched = await message.channel.messages.fetch({ limit: amount });
+            await message.channel.bulkDelete(fetched);
 
-        const embed = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle('Purge 👹')
-            .setDescription('How many messages do you want to delete?')
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
+            const embed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Purge Successful')
+                .setDescription(`Successfully deleted ${fetched.size} messages.`)
+                .setFooter({ text: 'Embotic', iconURL: message.client.user.displayAvatarURL() })
+                .setTimestamp();
 
-        const embedTimeout = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle('Purge 👹')
-            .setDescription('You did not input a number in time.')
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
+            const confirmationMessage = await message.channel.send({ embeds: [embed] });
 
-        const embedInvalidNumber = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle('Purge 👹')
-            .setDescription('Please enter a valid number.')
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
-
-        const embedOutOfRange = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle('Purge 👹')
-            .setDescription('You need to input a number between 1 and 100.')
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
-
-        const embedError = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle('Purge 👹')
-            .setDescription('There was an error trying to purge messages in this channel!')
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
-
-        message.channel.send({ embeds: [embed] }).then(sentMessage => {
-            setTimeout(() => sentMessage.delete(), 60000);
-
-            message.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-                .then(collected => {
-                    const response = collected.first();
-                    const amount = parseInt(response.content);
-
-                    if (isNaN(amount)) {
-                        message.channel.send({ embeds: [embedInvalidNumber] });
-                        return;
-                    } else if (amount < 1 || amount > 100) {
-                        message.channel.send({ embeds: [embedOutOfRange] }).then(sentMessage => {
-                            setTimeout(() => sentMessage.delete(), 60000);
-                        });
-                        return;
+            setTimeout(async () => {
+                try {
+                    if (confirmationMessage.channel) {
+                        await confirmationMessage.delete();
                     }
-
-                    message.channel.bulkDelete(amount, true).catch(err => {
-                        console.error(err);
-                        message.channel.send({ embeds: [embedError] });
-                    });
-                })
-                .catch(() => {
-                    message.channel.send({ embeds: [embedTimeout] }).then(sentMessage => {
-                        setTimeout(() => sentMessage.delete(), 60000);
-                    });
-                });
-        });
+                } catch (error) {
+                    console.error('Error deleting confirmation message:', error);
+                }
+            }, 5000);
+        } catch (error) {
+            console.error('Error purging messages:', error);
+            const embed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle('Error')
+                .setDescription('There was an error trying to purge messages.');
+            message.channel.send({ embeds: [embed] });
+        }
     },
 };
