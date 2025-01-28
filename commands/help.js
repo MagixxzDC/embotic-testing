5c5114b850d401dcc1cb8231a18bc7ac469222a5
Magixxz <dgarrett234@gmail.com>
2025-01-27 20:00:01 -0500

    Update

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,16 @@ module.exports = {
     name: 'help',
     description: 'Lists all available commands.',
     async execute(message, args) {
-        const commands = message.client.commands.map(command => `\`${command.name}\`: ${command.description}`).join('\n');
-        
+        const commands = message.client.commands.map(command => ({
+            name: command.name,
+            value: command.description || 'No description available.'
+        }));
+
         const helpEmbed = new EmbedBuilder()
             .setColor('#141414')
             .setTitle('Help :information_source:')
             .setDescription('Here are all the available commands:')
-            .addFields([{ name: 'Commands', value: commands }])
+            .addFields(commands)
             .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
             .setTimestamp();
 
