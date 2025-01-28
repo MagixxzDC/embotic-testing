3924ac812e598af903f4a5cb3847fd76ba492b05
Magixxz <dgarrett234@gmail.com>
2025-01-27 21:57:31 -0500

    Update

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'help',
     description: 'Lists all available commands.',
     async execute(message, args) {
-        const commands = message.client.commands.map(command => ({
+        const commands = message.client.commands.filter(command => !command.aliases).map(command => ({
             name: command.name.length > 256 ? command.name.slice(0, 253) + '...' : command.name,
             value: (command.description || 'No description available.').length > 1024 ? (command.description || 'No description available.').slice(0, 1021) + '...' : (command.description || 'No description available.')
         }));
@@ -13,16 +13,16 @@ module.exports = {
         let totalLength = 0;
         for (const command of commands) {
             const fieldLength = command.name.length + command.value.length;
-            if (totalLength + fieldLength > 6000) break; 
+            if (totalLength + fieldLength > 6000) break; // Discord's total embed length limit is 6000 characters
             fields.push(command);
             totalLength += fieldLength;
         }
 
         const helpEmbed = new EmbedBuilder()
-            .setColor(0x141414) // Use a valid integer value for color
+            .setColor('#141414')
             .setTitle('Help :information_source:')
             .setDescription('Here are all the available commands:')
-            .addFields(fields.slice(0, 25)) // Ensure no more than 25 fields are added
+            .addFields(fields)
             .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
             .setTimestamp();
 
