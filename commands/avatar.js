4833e88a9e2cbfd51d8a3afde0be2be000bf8b20
Magixxz <dgarrett234@gmail.com>
2025-01-28 21:15:55 -0500

    Update

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,18 +3,27 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     name: 'avatar',
     aliases: ['av', 'pfp', 'a'],
-    description: 'Displays the avatar of the mentioned user or the command user.',
+    description: 'Displays the avatar of the mentioned user or the command user. If a role is mentioned, displays avatars of all users with that role.',
     execute(message, args) {
-        let user = message.mentions.users.first() || message.author;
-        let avatarURL = user.displayAvatarURL({ dynamic: true, size: 1024 });
+        let users = [];
+        if (message.mentions.roles.size > 0) {
+            let role = message.mentions.roles.first();
+            users = role.members.map(member => member.user);
+        } else {
+            users.push(message.mentions.users.first() || message.author);
+        }
 
-        const avatarEmbed = new EmbedBuilder()
-            .setColor('#141414')
-            .setTitle(`${user.username}'s Avatar`)
-            .setImage(avatarURL)
-            .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
-            .setTimestamp();
+        users.forEach(user => {
+            let avatarURL = user.displayAvatarURL({ dynamic: true, size: 1024 });
 
-        message.channel.send({ embeds: [avatarEmbed] });
+            const avatarEmbed = new EmbedBuilder()
+                .setColor('#141414')
+                .setTitle(`${user.username}'s Avatar`)
+                .setImage(avatarURL)
+                .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
+                .setTimestamp();
+
+            message.channel.send({ embeds: [avatarEmbed] });
+        });
     },
 };
