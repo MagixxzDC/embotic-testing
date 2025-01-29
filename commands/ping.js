083663f79bb7971129c2b45c6c01ce61957c9547
Magixxz <dgarrett234@gmail.com>
2025-01-28 19:27:40 -0500

    Update

--- commands/ping.js
+++ commands/ping.js
@@ -2,14 +2,14 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'ping',
-    aliases: ['latency', 'ms', 'pong', 'hello', 'test'],
+    aliases: ['test', 'latency', 'ms', 'pong', 'hello', 'test'],
     description: 'Ping command',
     execute(message, args) {
         const latency = Date.now() - message.createdTimestamp;
         const embed = new EmbedBuilder()
             .setColor('#141414')
             .setTitle('Ping')
-            .setDescription(`Pong! Latency: ${latency}ms.`)
+            .setDescription(`Pong! :ping_pong: | Latency: **${latency}ms.**`)
             .setFooter({ text: 'Embotic', iconURL: message.guild.iconURL() })
             .setTimestamp();
 
