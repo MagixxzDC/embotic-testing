3e108fcc86e20fce4bbe64e2ea5ef0ba2d2f439a
Magixxz <dgarrett234@gmail.com>
2025-01-28 19:22:48 -0500

    Update

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -7,6 +7,7 @@ module.exports = {
     execute(message, args) {
         const authorizedUserId = '328248785933434881';
         const channelId = '814759943789215755';
+        const secondChannelId = '838514683253620787'; // New channel ID
         
         if (message.author.id !== authorizedUserId) {
             const embed = new EmbedBuilder()
@@ -24,9 +25,31 @@ module.exports = {
             setTimeout(() => {
                 sentMessage.delete();
                 const channel = message.guild.channels.cache.get(channelId);
+                const secondChannel = message.guild.channels.cache.get(secondChannelId); // Get the second channel
+                
                 if (channel) {
-                    channel.setName('OFFLINE 🔴').then(() => {
-                        process.exit();
+                    channel.setName('Offline 🔴').then(() => {
+                        if (secondChannel) {
+                            secondChannel.setName('🕒 | Offline').then(() => {
+                                process.exit();
+                            }).catch(err => {
+                                const errorEmbed = new EmbedBuilder()
+                                    .setColor('#141414')
+                                    .setTitle('Error')
+                                    .setDescription(`Failed to rename the second channel: ${err.message}`);
+                                message.channel.send({ embeds: [errorEmbed] }).then(() => {
+                                    process.exit();
+                                });
+                            });
+                        } else {
+                            const errorEmbed = new EmbedBuilder()
+                                .setColor('#141414')
+                                .setTitle('Error')
+                                .setDescription('Second channel not found');
+                            message.channel.send({ embeds: [errorEmbed] }).then(() => {
+                                process.exit();
+                            });
+                        }
                     }).catch(err => {
                         const errorEmbed = new EmbedBuilder()
                             .setColor('#141414')
